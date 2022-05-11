39678b5dbb504b228edf411c204e4aa52c231a66
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2022-05-11 11:15:31 +0200

    Add files via upload

--- functionApp/onboarding/run.ps1
+++ functionApp/onboarding/run.ps1
@@ -3,69 +3,64 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-Import-Module ".\Modules\HelperFunctions.psm1" -Force -Verbose:$false
-
 $processId = $Request.Query.ProcessId
 Write-Output $processId
 Write-Output $Request
-if ($processId -ne $null){
+if (-not([string]::IsNullOrWhiteSpace($processId))) {
 
+    try {
+        $payload = $($Request.RawBody) | ConvertFrom-Json
+    }
+    catch {
+        $body = "Invalid payload"
+    }
 
+    try {
+        $KeyVaultName = "$env:keyVaultName"
+        $offSet = $processId -replace "[a-zA-Z -]"
 
-try {
-    $payload = $($Request.RawBody) | ConvertFrom-Json
-}
-catch {
-    $body = "Invalid payload"
-}
+        $prefix = Get-Prefix -processId $processId -value $payload.a
+        Write-Output "Prefix Value: [$($prefix)]"
 
-try {
-    
-    $KeyVaultName = "$env:keyVaultName"
-    $offSet = $processId -replace "[a-zA-Z -]"
+        Write-Output "Setting Application ID"
+        Set-AzKeyVaultSecret `
+            -VaultName $KeyVaultName `
+            -SecretName ($prefix + '-' + "applicationid") `
+            -SecretValue ( ConvertTo-Key `
+                -value $payload.b `
+                -KeySet $processId `
+                -subKey 1
+        )
 
-    $prefix = Get-Prefix -processId $processId -value $payload.a
-    Write-Output "Prefix Value: [$($prefix)]"
-    
-    Write-Output "Setting Application ID"
-    Set-AzKeyVaultSecret `
-        -VaultName $KeyVaultName `
-        -SecretName ($prefix + '-' + "applicationid") `
-        -SecretValue ( ConvertTo-Key `
-                        -value $payload.b `
-                        -KeySet $processId `
-                        -subKey 1
-                    )
+        Write-Output "Setting Application Key"
+        Set-AzKeyVaultSecret `
+            -VaultName $KeyVaultName `
+            -SecretName ($prefix + '-' + "applicationkey") `
+            -SecretValue ( ConvertTo-Key `
+                -value $payload.c `
+                -KeySet $processId `
+                -subKey 2
+        )
 
-    Write-Output "Setting Application Key"
-    Set-AzKeyVaultSecret `
-        -VaultName $KeyVaultName `
-        -SecretName ($prefix + '-' + "applicationkey") `
-        -SecretValue ( ConvertTo-Key `
-                        -value $payload.c `
-                        -KeySet $processId `
-                        -subKey 2
-                    )
+        Write-Output "Setting Tenant ID"
+        Set-AzKeyVaultSecret `
+            -VaultName $KeyVaultName `
+            -SecretName ($prefix + '-' + "tenantid") `
+            -SecretValue ( ConvertTo-Key `
+                -value $payload.d `
+                -KeySet $processId `
+                -subKey 3
+        )
 
-    Write-Output "Setting Tenant ID"
-    Set-AzKeyVaultSecret `
-    -VaultName $KeyVaultName `
-    -SecretName ($prefix + '-' + "tenantid") `
-    -SecretValue ( ConvertTo-Key `
-                    -value $payload.d `
-                    -KeySet $processId `
-                    -subKey 3
-                )
+        $body = "Succesfully processed request for customer ID: [$($prefix)]"
+    }
+    catch {
+        $body = "Unable to process the request, please contact support for customer ID: [$($prefix)]"
+    }
 
-    $body = "Succesfully processed request for customer ID: [$($prefix)]"
-}
-catch {
-    $body = "Unable to process the request, please contact support for customer ID: [$($prefix)]"
-}
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
-})
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body       = $body
+        })
 }
